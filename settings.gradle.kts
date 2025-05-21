40469475aa151777a5055a0d2cabdc7226adcffe
Hayden Baker <haydenbaker98@gmail.com>
2025-05-21 12:40:07 -0700

    Create subprojects based on presence versus from properties, Sync models (#8)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,10 @@ pluginManagement {
     }
 }
 
-// Dynamically create subprojects for each model defined in the properties file
-settings.extra.properties.forEach {
-    if (it.key.startsWith("model.") && it.key.endsWith(".version")) {
-        val project = it.key.substring(6, it.key.length - 8)
-        // Skip the "all" project, it is set up in the root gradle file
-        if (project != "all") {
-            include(project)
-            project(":${project}").projectDir = file("models/${project}")
-        }
+// Dynamically create subprojects for each directory in models/
+fileTree("models/").files.forEach {
+    if (it.isDirectory && !it.isHidden) {
+        include(it.name)
+        project(":${it.name}").projectDir = it
     }
 }
